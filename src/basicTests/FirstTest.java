4086fb9bbedabaf0291d03a097ba3e08a64c69d5
Karl <karllamoureux2@gmail.com>
2020-07-14 13:12:02 -0400

    src commit

---
+++ src/basicTests/FirstTest.java
@@ -0,0 +1,13 @@
+package basicTests;
+
+import org.openqa.selenium.WebDriver; 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+
+	}
+
+}
